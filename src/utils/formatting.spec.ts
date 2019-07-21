8768317d9d1eef71566b26d2af5b0adccf5e36b7
Greg Murray <greg.murray@volusion.com>
2019-07-21 18:16:33 -0500

    fix toPascalCase

--- src/utils/formatting.spec.ts
+++ src/utils/formatting.spec.ts
@@ -3,6 +3,7 @@ import { formatName, toPascalCase } from "./index";
 const anotherString = "this is another string";
 const aString = "a string";
 const word = "word";
+const pascalCase = "PascalCase";
 
 describe("formatName", () => {
     it("upper cases the first letter of each word in a string", () => {
@@ -15,6 +16,10 @@ describe("formatName", () => {
 describe("toPascalCase", () => {
     it("upper-cases a single word", () => {
         expect(toPascalCase(word)).toEqual("Word");
+    });
+
+    it("does not modify a word already in Pascal Case", () => {
+        expect(toPascalCase(pascalCase)).toEqual(pascalCase);
     });
 
     it("converts a string of more than two words", () => {
--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -10,6 +10,14 @@ export const formatName = (input: string): string => {
     return upperCaseFirsts(input, " ");
 };
 
-export const toPascalCase = (input: string): string => {
-    return upperCaseFirsts(input, "");
-};
+// from https://stackoverflow.com/a/53952925/753279
+export const toPascalCase = (input: string): string =>
+    `${input}`
+        .replace(new RegExp(/[-_]+/, "g"), " ")
+        .replace(new RegExp(/[^\w\s]/, "g"), "")
+        .replace(
+            new RegExp(/\s+(.)(\w+)/, "g"),
+            (_$1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`
+        )
+        .replace(new RegExp(/\s/, "g"), "")
+        .replace(new RegExp(/\w/), s => s.toUpperCase());
