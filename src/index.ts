65b9c03295ddf2e6d0abbe5b9f2074c3453cfb56
Greg Murray <greg.murray@volusion.com>
2020-05-10 11:27:44 -0500

    update version

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ import {
 import { getCategoryNames, logError, logInfo } from "./utils";
 
 program
-    .version("3.0.8", "-v, --version")
+    .version("3.0.9", "-v, --version")
     .usage(`[options] command`)
     .option("-V, --verbose", "Display verbose output")
     .description("Command line interface for the Volusion Element ecosystem");
